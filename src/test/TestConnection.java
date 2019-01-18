877c384ab7ba0d8531e53bf23886b0769261b0ef
TNB-20 <rogertim3@gmail.com>
2019-01-18 16:57:23 +0100

    Check Online

---
+++ src/test/TestConnection.java
@@ -0,0 +1,19 @@
+package test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		try {
+			EntityManagerFactory factory = Persistence.createEntityManagerFactory("ScrumHibernate");
+	        EntityManager entityManager = factory.createEntityManager();
+	        System.out.println("ONLINE");
+		}catch(Exception e) {
+			System.err.println("No funciona la conexión");
+		}
+	}
+
+}
