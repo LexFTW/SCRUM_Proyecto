c063a952eb60840fef19c19082c4fa721b2f99c7
LexFTW <lexmengual@gmail.com>
2019-01-20 22:59:24 +0100

    Error MariaDB Solved

---
+++ src/main/implementations/UserSQLRemote.java
@@ -0,0 +1,51 @@
+package main.implementations;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import main.interfaces.IUser;
+import main.models.User;
+
+public class UserSQLRemote implements IUser{
+
+	@Override
+	public void loadUsers() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("ScrumHibernate");
+        EntityManager entityManager = factory.createEntityManager();
+		int primaryKey = 1;
+		while(entityManager.find(User.class, primaryKey) != null) {
+			this.users.add(entityManager.find(User.class, primaryKey));
+			primaryKey++;
+		}
+	}
+
+	@Override
+	public String getHashingPassword(String password) {
+		MessageDigest md = null;
+		byte[] result = null;
+		try {
+			result = md.digest(password.getBytes("UTF8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        
+        return sb.toString();
+	}
+
+	@Override
+	public void getAllUsers() {
+		for (User user : users) {
+			System.out.println(user.toString());
+		}
+	}
+
+}
