e1d600dd7e5105f5f11c601d52ef241abd0f1d6c
TNB-20 <rogertim3@gmail.com>
2019-01-21 17:10:37 +0100

    subida de los modelos acabados

--- src/main/models/UserGroup.java
+++ src/main/models/UserGroup.java
@@ -1,5 +1,53 @@
 package main.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users_groups")
 public class UserGroup {
 
+	
+	private int groupID;
+	private String groupName;
+	private Date createdAt;
+	private Date updateAt;
+	
+	@Column(name="GroupID")
+	public int getGroupID() {
+		return groupID;
+	}
+	public void setGroupID(int groupID) {
+		this.groupID = groupID;
+	}
+	@Column(name="GroupName")
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	@Column(name="CreatedAt")
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		createdAt = createdAt;
+	}
+	
+	@Column(name="UpdatedAt")
+	public Date getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(Date updateAt) {
+		updateAt = updateAt;
+	}
+	
+	
+	
+	
+	
 }
