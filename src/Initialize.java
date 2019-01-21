52e8f918151d6cf2b33037c04bb7d9807bdf9441
LexFTW <lexmengual@gmail.com>
2019-01-21 16:52:05 +0100

    Initialize Class

---
+++ src/Initialize.java
@@ -0,0 +1,27 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import main.implementations.UserSQLRemote;
+import main.interfaces.IUser;
+import main.views.Login;
+
+public class Initialize {
+	
+	private static IUser iuser;
+
+	public static void main(String[] args) {
+		try {
+			EntityManagerFactory factory = Persistence.createEntityManagerFactory("ScrumSQLRemote");
+	        EntityManager entityManager = factory.createEntityManager();
+	        System.out.println("[INFO] - Conexión Online");
+	        iuser = new UserSQLRemote();
+		}catch(Exception e) {
+			// Connection to SQL Local:
+			System.out.println("[INFO] - Conexión Offline");
+		}
+		
+		Login login = new Login(iuser);
+	}
+
+}
